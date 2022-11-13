e8e78140daf4788fc41e8cd60360aaeb4cc9bb6e
尋。香港 <lookforhk1210@gmail.com>
2022-11-13 19:34:17 +0800

    setting profile website

---
+++ component/Profile Website/script/app.js
@@ -0,0 +1,34 @@
+// javasrcipt 
+
+const menuBtn = document.querySelector(".toggler");
+const menuLink = document.querySelector(".navbar-navlinks");
+const body = document.querySelector("body-setting");
+
+menuBtn.addEventListener("click", () => {
+    toggle();
+
+    /* freeze screen */
+    freeze();
+});
+
+
+function toggle() {
+    menuBtn.classList.toggle("open");
+    menuLink.classList.toggle("open");
+}
+
+function freeze() {
+   body.classList.toggle("freeze")
+}
+
+document.querySelector(".navbar-navlink").forEach(item => {
+   item.addEventListener("click", event => {
+    activeLink();
+   })
+})
+
+function activeLink() {
+    menuBtn.classList.remove("open")
+    menuLink.classList.remove("open");
+    document.getElementById("toggler").checked = false;
+}
