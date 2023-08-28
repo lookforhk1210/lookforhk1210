2b4b2285f5c1db5ea75d8651b67bbc13f737208e
尋。香港 <lookforhk1210@gmail.com>
2023-08-28 22:40:54 +0800

    update profile website effect and fixed bug

--- component/profile/script/main.js
+++ component/profile/script/main.js
@@ -60,7 +60,7 @@ sr.reveal('.main-skills', {
     delay: 100
 });
 
-sr.reveal('.course-col',{
+sr.reveal('.course-col', {
     interval: 50,
 });
 
@@ -76,3 +76,75 @@ sr.reveal('.project-card-col', {
 sr.reveal('.main-contact', {
     delay: 100
 });
+
+sr.reveal('.skills-row', {
+    interval: 140,
+    origin: 'left'
+})
+
+/********************************************* */
+var targetPercentages = {
+    html: 80,
+    css: 80,
+    javascript: 60,
+    reactjs: 50,
+    swiftui: 30,
+    ai: 80,
+    ps: 70,
+    lightroom: 80,
+    pr: 70,
+    figma: 80
+};
+
+var duration = 6800;
+var framesPerSecond = 30;
+
+function updatePercentage(elementId, targetPercentage) {
+    var element = document.getElementById(elementId);
+    var currentPercentage = parseInt(element.textContent);
+
+    var increment = (targetPercentage - currentPercentage) / (duration / 1000) / framesPerSecond;
+    var counter = currentPercentage;
+
+    function update() {
+        if (counter < targetPercentage) {
+            counter += increment;
+            element.textContent = Math.round(counter) + "%";
+            requestAnimationFrame(update);
+        } else {
+            element.textContent = targetPercentage + "%";
+        }
+    }
+
+    update();
+}
+
+function handleScroll() {
+    var skillsSection = document.getElementById('skills');
+
+    if (isElementInViewport(skillsSection)) {
+        window.removeEventListener('scroll', handleScroll);
+        updatePercentage("html-skill-row", targetPercentages.html);
+        updatePercentage("css-skill-row", targetPercentages.css);
+        updatePercentage("javascript-skill-row", targetPercentages.javascript);
+        updatePercentage("reactjs-skill-row", targetPercentages.reactjs);
+        updatePercentage("swiftui-skill-row", targetPercentages.swiftui);
+        updatePercentage("ai-skill-row", targetPercentages.ai);
+        updatePercentage("ps-skill-row", targetPercentages.ps);
+        updatePercentage("lightroom-skill-row", targetPercentages.lightroom);
+        updatePercentage("pr-skill-row", targetPercentages.pr);
+        updatePercentage("figma-skill-row", targetPercentages.figma);
+    }
+}
+
+window.addEventListener('scroll', handleScroll);
+
+function isElementInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
